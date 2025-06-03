cd7fff0e9be1b38afdf1df543b9214fd167671d5
Tech-Savvy <wilkinsondari7@gmail.com>
2025-06-03 18:44:52 +0300

    Update extractPass.py

--- extractPass.py
+++ extractPass.py
@@ -1,18 +1,32 @@
 import os
 import pyzipper
 import getpass
+import platform
 
-zip_file_path=f'c:/Users/{getpass.getuser()}/Favorites/Backup.zip'
-extract_folder = os.getcwd()+"\\"+"production"
+if 'windows' in platform.platform().lower():
+    zip_file_path = f'c:Users/{getpass.getuser()}/Favorites/.backup/Backup.zip'
+elif 'linux' in platform.platform().lower():
+    zip_file_path =  f'/home/{getpass.getuser()}/.backup/Backup.zip'
+
+
+
+extract_folder = "production1"
 
 
 def extract_zip_with_password(zip_file_path, extract_folder, password):
+    if not os.path.exists(zip_file_path):
+        print('Backup does not exist!'.capitalize())
+        exit()
     if not os.path.exists(extract_folder):
         os.makedirs(extract_folder)
     with pyzipper.AESZipFile(zip_file_path, 'r') as zip_file:
-        zip_file.setpassword(password)
-        zip_file.extractall(extract_folder)
+        try:
+            zip_file.setpassword(password)
+            zip_file.extractall(extract_folder)
+            print("Backup successfully extracted to production1")
+        except Exception as e:
+            print(f'An error occured: {e}')    
 
 if __name__ == "__main__":
-    password = input('Enter password to Unlock file (Hint: cuzz + favorite band): '.capitalize()).encode('utf-8')
+    password = input('Enter password to Unlock file: '.capitalize()).encode('utf-8')
     extract_zip_with_password(zip_file_path, extract_folder, password)
